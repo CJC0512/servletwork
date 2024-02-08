969c3b893a59417952cf36e5b0e701d1a51fbd86
CJC0512 <ehdchs2006@gmail.com>
2024-02-08 11:47:47 +0900

    chap11 uses 작성2

---
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section02.uses;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/member/regist")
+public class RegistMemberServlet extends HttpServlet {
+    public RegistMemberServlet() {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+
+        System.out.println("userId = " + userId);
+        System.out.println("password = " + password);
+        System.out.println("name = " + name);
+//        StringBuilder sb = new StringBuilder();
+//        PrintWriter out = resp.getWriter();
+
+    }
+}
